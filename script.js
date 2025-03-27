9f5879c6bb55d82a0ce414eab75422d0f730b3a9
eskaykaushik <eskaykaushik14@gmail.com>
2025-03-27 16:36:42 +0530

    API added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  
-const API_URL = "http://127.0.0.1:5000"; // Replace with your actual backend URL
+const API_URL = "https://backendserviceflask.onrender.com/"; // Replace with your actual backend URL
 
 async function checkSpam() {
     const emailText = document.getElementById("emailText").value.trim();
